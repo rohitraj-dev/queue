aa80e130665f386f6c7cfe64e8b3ee494fe559a9
Rohit Raj <rajrohit7388@outlook.com>
2026-09-08 23:57:06 +0530

    Add files via upload

---
+++ ImplementQueueUsingStack.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class MyQueue
+{
+    Stack<Integer> st = new Stack<>();
+    Stack<Integer> helper = new Stack<>();
+    public MyQueue()
+    {}
+    public void push(int x)
+    {
+        while(st.size()>0)
+        {
+            helper.push(st.pop());
+        }
+        st.push(x);
+        while(helper.size()>0)
+        {
+            st.push(helper.pop());
+        }
+    }
+    public int pop()
+    {
+        return st.pop();
+    }
+    public int peek()
+    {
+        return st.peek();
+    }
+    public boolean empty()
+    {
+        return (st.size() == 0);
+    }
+}
